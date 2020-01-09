35bcd671857ee7271c3f15afb534d378bf00ab59
Phumthawan <knzphumthawan@gmail.com>
2020-01-09 07:03:40 +0700

    type annotations and type inference

--- feature/anotations/variables.ts
+++ feature/anotations/variables.ts
@@ -1,4 +1,4 @@
-const apples: number = 5;
+let apples = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 let nothingmush: null = null;
@@ -22,3 +22,32 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+// function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+//when use annotations
+//1. functions that returns the 'any' type
+const json = '{"x":10,"y":20}';
+const coordinate: { x: number; y: number } = JSON.parse(json);
+console.log(coordinate);
+
+//2. when we declae a variable on one line and initalizate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+//3. Variable whose type connot be infered correctly
+let numbers = [-1, -10, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  numberAboveZero = numbers[i];
+}
