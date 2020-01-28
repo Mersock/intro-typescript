8c1fecc2b5f46a5bd7df0fb68d330438c6f2a9e2
knzphumthawan <knzphumthawan@gmail.com>
2020-01-28 07:10:16 +0700

    add csv file reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,23 @@
-import fs from 'fs';
+import { CsVFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsVFileReader('football.csv');
+reader.read();
 
-console.log(matches);
+//enum - enumeration
+enum matchResult {
+  homeWin = 'H',
+  awayWin = 'A',
+  draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (const match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === matchResult.homeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === matchResult.awayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
