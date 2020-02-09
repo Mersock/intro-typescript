f10f2d26643342e5deb1c824d9b6437d71673589
knzphumthawan <knzphumthawan@gmail.com>
2020-02-09 10:23:39 +0700

    making collection

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,10 @@
 import { Collection } from './models/collection';
+import { User, UserProps } from './models/user';
 
-const collection = new Collection('http://localhost:3000/users');
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on('change', () => {
   console.log(collection);
