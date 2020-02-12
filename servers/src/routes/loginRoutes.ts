af0b92b78dffa3e8d80ae0a1a6f9ac6ebe50a723
knzphumthawan <knzphumthawan@gmail.com>
2020-02-13 05:32:27 +0700

    add login logout route

--- servers/src/routes/loginRoutes.ts
+++ servers/src/routes/loginRoutes.ts
@@ -1,10 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 /**
  * define request with body.
  */
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  res.status(403).send('Not Permission');
 }
 
 const router = Router();
@@ -58,6 +66,10 @@ router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
 
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route.');
 });
 
 export { router };
