15b11bf67ae4e798604c118960fbf49855cc4649
knzphumthawan <knzphumthawan@gmail.com>
2020-02-16 12:11:01 +0700

    meta data pattern

--- feature/metadata.ts
+++ feature/metadata.ts
@@ -10,8 +10,35 @@ const plane = {
 // const note = Reflect.getMetadata('note', plane);
 // const height = Reflect.getMetadata('height', plane);
 
-Reflect.defineMetadata('note', 'hi', plane, 'color');
+// Reflect.defineMetadata('note', 'hi', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note);
+// console.log(note);
+
+@controller
+class Plane {
+  color: string = 'red';
+
+  @get('hello')
+  fly(): void {
+    console.log('fly');
+  }
+}
+
+function get(path: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+const secret = Reflect.getMetadata('path', Plane.prototype, 'fly');
+
+function controller(target: typeof Plane) {
+  for (const key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    console.log(path);
+  }
+}
