2a5376ca25da6d58502b42b92c25dd28ae496377
knzphumthawan <knzphumthawan@gmail.com>
2020-02-16 17:17:35 +0700

    define enum method

--- servers/src/controllers/decorators/routes.ts
+++ servers/src/controllers/decorators/routes.ts
@@ -1,9 +1,20 @@
 import 'reflect-metadata';
+import { Methods } from './method';
 
-export function get(path: string) {
-  return function(target: any, key: string) {
-    Reflect.defineMetadata('path', path, target, key);
-    // console.log('method.get', target);
-    // console.log('method.get', key);
+function routeBinder(method: string) {
+  return function(path: string) {
+    return function(target: any, key: string) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+
+      // console.log('method.', target);
+      // console.log('method.get', key);
+    };
   };
 }
+
+export const get = routeBinder(Methods.get);
+export const put = routeBinder(Methods.put);
+export const post = routeBinder(Methods.post);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
